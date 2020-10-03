38e80c1c3d05a145c56d1ba5590583417878c5ed
Chris Skaaning <c@rpediem.com>
2020-10-03 22:21:50 +0100

    Changed all the function names

--- deployment/lambda.tf
+++ deployment/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_permission" "Yogalates-classes" {
 
 resource "aws_lambda_function" "Yogalates-classes" {
   filename      = "../artifact/artifact.zip"
-  function_name = "Yogalates-courses"
+  function_name = "Yogalates-classes"
   role          = "arn:aws:iam::368263227121:role/BasicLambdaRole"
   handler       = "index.handler"
   source_code_hash = "${filebase64sha256("../artifact/artifact.zip")}"
