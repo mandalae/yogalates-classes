2aaf3426bc88209d67629ae2cf5ade239127212b
Chris Skaaning <c@rpediem.com>
2020-10-03 22:52:57 +0100

    Added saveClass and deleteClass functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const aws = require('aws-sdk');
 
+const dynamo = new aws.DynamoDB();
 const docClient = new aws.DynamoDB.DocumentClient();
+
+const tableName = 'yogalates-classes';
 
 exports.handler = async (event) => {
     return new Promise(async (resolve, reject) => {
@@ -27,7 +30,7 @@ exports.handler = async (event) => {
         switch (event.httpMethod) {
             case 'GET':
                 const params = {
-                    TableName: "yogalates-classes"
+                    TableName: tableName
                 }
                 docClient.scan(params, (err, data) => {
                     let items = [];
@@ -36,6 +39,56 @@ exports.handler = async (event) => {
                     }
                     console.log(err, items);
                     done(null, items);
+                });
+                break;
+            case 'POST':
+                const pageObject = JSON.parse(event.body);
+
+                const params = {
+                    TableName: tableName,
+                    Item:{
+                        'slug': classObject.slug,
+                        'time': classObject.time,
+                        'text': classObject.text,
+                        'postCode': classObject.postCode,
+                        'name': classObject.name,
+                        'link': classObject.link,
+                        'city': classObject.city,
+                        'address1': classObject.address1,
+                        'address2': classObject.address2
+                    }
+                };
+
+                docClient.put(params, async (err, data) => {
+                    if (err) {
+                        console.log("ERR: ", err);
+                        done('Unable to create item. Error JSON: ' + JSON.stringify(err));
+                    } else {
+                        console.log("PutItem succeeded:", JSON.stringify(data, null, 2));
+                        const clearCacheResult = await clearCache('Prod');
+                        console.log(clearCacheResult);
+                        done(null, {success: true});
+                    }
+                });
+                break;
+            case 'DELETE':
+                const classSlug = event.pathParameters.classSlug;
+
+                const params = {
+                    Key: {
+                        "slug": {
+                            S: classSlug
+                        }
+                    },
+                    TableName: tableName
+                }
+                dynamo.deleteItem(params, async (err, res) => {
+                    if (err){
+                        done(err);
+                    } else {
+                        console.log(res);
+                        done(null, {success: true});
+                    }
                 });
                 break;
             default:
