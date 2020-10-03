82b14dfe481ed87f7082047d2a16ac51d6fe971c
Chris Skaaning <c@rpediem.com>
2020-10-03 22:09:27 +0100

    Added classes backend

---
+++ deployment/lambda.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_lambda_permission" "Yogalates-classes" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.Yogalates-classes.function_name}"
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_function" "Yogalates-classes" {
+  filename      = "../artifact/artifact.zip"
+  function_name = "Yogalates-courses"
+  role          = "arn:aws:iam::368263227121:role/BasicLambdaRole"
+  handler       = "index.handler"
+  source_code_hash = "${filebase64sha256("../artifact/artifact.zip")}"
+  runtime       = "nodejs12.x"
+}
